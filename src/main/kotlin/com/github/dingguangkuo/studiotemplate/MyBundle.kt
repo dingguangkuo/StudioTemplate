47116dec44678c41eb9c54b826ea68d0b3ddad5f
GitHub Action <action@github.com>
2021-06-16 00:39:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/dingguangkuo/studiotemplate/MyBundle.kt
+++ src/main/kotlin/com/github/dingguangkuo/studiotemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dingguangkuo.studiotemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/dingguangkuo/studiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dingguangkuo.studiotemplate.services
+
+import com.github.dingguangkuo.studiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
